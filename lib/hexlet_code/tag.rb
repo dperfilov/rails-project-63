9017387d7a16186db97e4ae9bca3b8ed6462711c
Denis <dperfilov@softmart.ru>
2024-02-23 12:16:10 +0300

    rubocop autofix

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,34 +1,34 @@
 # frozen_string_literal: true
 
 module HexletCode
-	class Tag
-	  def self.build(tag_name, params = {}, fields = [])
-	    # void elements area, base, br, col, embed, hr, img, input, link, meta, source, track, wbr
-	    unpaired = %w[br hr img input link]
+  class Tag
+    def self.build(tag_name, params = {}, fields = [])
+      # void elements area, base, br, col, embed, hr, img, input, link, meta, source, track, wbr
+      unpaired = %w[br hr img input link]
 
-	    result = []
-	    result << '<'
-	    result << tag_name
+      result = []
+      result << '<'
+      result << tag_name
 
-	    # add attributes from hash
-	    unless params.empty?
-	      params.each do |key, value|
-	        result << " #{key}=\"#{value}\""
-	      end
-	    end
+      # add attributes from hash
+      unless params.empty?
+        params.each do |key, value|
+          result << " #{key}=\"#{value}\""
+        end
+      end
 
-	    result << '>'
+      result << '>'
 
-	    # add body from given block
-	    result << yield if block_given?
+      # add body from given block
+      result << yield if block_given?
 
-	    # add fields as form content
-	    result << fields unless fields.empty?
+      # add fields as form content
+      result << fields unless fields.empty?
 
-	    # for paired tags add closing part
-	    result << "</#{tag_name}>" unless unpaired.include? tag_name
+      # for paired tags add closing part
+      result << "</#{tag_name}>" unless unpaired.include? tag_name
 
-	    result.join
-	  end
-	end
+      result.join
+    end
+  end
 end
